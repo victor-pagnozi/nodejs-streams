29beef32be20f652c07831d81969c0e8d2f62a01
victor-pagnozi <victor.pagnozi@gmail.com>
2024-01-17 13:32:06 -0300

    Converted traditional mode to read file, to streams

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import { readFile } from "fs";
+const fs = require('fs');
 const server = require("http").createServer();
 
-server.on("request", (req, res) => {
-  readFile("./big.file", (err, data) => {
-    if (err) throw err;
+// traditional model
+// fs("./big.file", (err, data) => {
+//   if (err) throw err;
 
-    res.end(data);
-  });
+//   res.end(data);
+// });
+// https://www.freecodecamp.org/portuguese/news/streams-em-node-js-tudo-o-que-voce-precisa-saber/
+
+server.on("request", (req, res) => {
+  const src = fs.createReadStream('./big.file');
+  src.pipe(res);
 });
 
-server.listen(8000);
+server.listen(8001);
